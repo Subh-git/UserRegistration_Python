9ce66b1d8b37b19ff972a0805a6fd0d6c42cfd49
Subhadeep <subhadeep0326@gmail.com>
2021-12-01 22:47:28 +0530

    [Subhadeep] add Email samples separate file containing valid and invalid samples.

---
+++ EmailSamples.py
@@ -0,0 +1,13 @@
+'''	
+	@Author: Subhadeep Bhattacharjee
+	@Date: 2021-12-01 22:10
+	@Title : Email samples for Testing 
+	
+'''
+
+ValidEmails = ["abc@gmail.com","1.abc@yahoo.com","2.abc-100@yahoo.com","3.abc.100@yahoo.com",
+                "2.abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com","7.abc@gmail.com.com","8.abc+100@gmail.com"]
+
+
+InValidEmails = ["abc","abc@.com.my","abc123@gmail.a","abc123@.com","abc123@.com.com",".abc@abc.com","abc()*@gmail.com",
+                "abc@%*.com","abc.@gmail.com","abc@abc@gmail.com","abc@gmail.com.1a","abc@gmail.com.aa.au"]
