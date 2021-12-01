af10805d8141c2420e66f1a88d27b958d9dd4387
Subhadeep <subhadeep0326@gmail.com>
2021-12-01 22:03:04 +0530

    [Subhadeep] add email validation method testing

--- Test_UserRegistrationForm.py
+++ Test_UserRegistrationForm.py
@@ -10,6 +10,9 @@ import UserRegistrationForm
 class Test_UserRegistration(unittest.TestCase):
 
     def test_first_name(self):
+        '''
+        This method is to test the validity of first name. It checks whether the called function returns valid or invalid.
+        '''
         self.assertEqual(UserRegistrationForm.first_name('Subhadeep'),'Valid')
         self.assertEqual(UserRegistrationForm.first_name('subhadeep'),'Invalid')
         self.assertEqual(UserRegistrationForm.first_name('Rahul'),'Valid')
@@ -17,10 +20,21 @@ class Test_UserRegistration(unittest.TestCase):
         self.assertEqual(UserRegistrationForm.first_name('Subhadeep Bhatta'),'Invalid')
 
     def test_last_name(self):
+        '''
+        This method is to test the validity of last name. It checks whether the called function returns valid or invalid.
+        '''
         self.assertEqual(UserRegistrationForm.last_name("Bhattacharjee"),"Valid")
         self.assertEqual(UserRegistrationForm.last_name("Gaikwad"),"Valid")
         self.assertEqual(UserRegistrationForm.last_name("Bh"),"Invalid")
         self.assertEqual(UserRegistrationForm.last_name("bhattacharjee"),"Invalid")
+
+    def test_email(self):
+        '''
+        This method is to test the validity of the email address. It checks whether the called function returns valid or invalid.
+        '''
+        self.assertEqual(UserRegistrationForm.email_name("Subhadeep@gmail.com"),"Valid")
+        self.assertEqual(UserRegistrationForm.email_name("Subhadeep.com"),"Invalid")
+        self.assertEqual(UserRegistrationForm.email_name("Subhadeep@gmail"),"Invalid")
 
 
 
