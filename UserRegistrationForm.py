4f2df73dc1db02fccda6e99deafd7e2e3beb23b0
Subhadeep <subhadeep0326@gmail.com>
2021-12-01 21:42:15 +0530

    [Subhadeep] add first name user registration

---
+++ UserRegistrationForm.py
@@ -0,0 +1,25 @@
+'''	
+	@Author: Subhadeep Bhattacharjee
+	@Date: 2021-12-01 17:00
+	@Title : User Registration Form
+	
+'''
+import re
+
+
+def first_name(string):
+    '''
+    Description:
+        This method is used to validate the first name entered.
+    Parameter:
+        This takes a string as parameter.
+    Return:
+        This returns whether the string is Valid or Invalid.
+    '''
+    reg = "^[A-Z]{1}[a-zA-Z]{2,}$"
+    
+    if (re.match(reg,string)):
+        return "Valid"
+    else:
+        return "Invalid"
+
