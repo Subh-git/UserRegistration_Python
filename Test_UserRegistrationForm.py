3a6ffe48789585e13042bfa728ad480ef641a09c
Subhadeep <subhadeep0326@gmail.com>
2021-12-01 21:41:55 +0530

    [Subhadeep] add unit testing of first name

---
+++ Test_UserRegistrationForm.py
@@ -0,0 +1,24 @@
+'''	
+	@Author: Subhadeep Bhattacharjee
+	@Date: 2021-12-01 17:10
+	@Title : Unit Testing For User Registration Form 
+	
+'''
+import unittest
+import UserRegistrationForm
+
+class Test_UserRegistration(unittest.TestCase):
+
+    def test_first_name(self):
+        self.assertEqual(UserRegistrationForm.first_name('Subhadeep'),'Valid')
+        self.assertEqual(UserRegistrationForm.first_name('subhadeep'),'Invalid')
+        self.assertEqual(UserRegistrationForm.first_name('Rahul'),'Valid')
+        self.assertEqual(UserRegistrationForm.first_name('Ro'),'Invalid')
+        self.assertEqual(UserRegistrationForm.first_name('Subhadeep Bhatta'),'Invalid')
+
+    
+
+
+
+if __name__ == "__main__":
+    unittest.main()
